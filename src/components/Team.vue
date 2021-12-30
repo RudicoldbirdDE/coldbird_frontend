249edefd8225ce033a5c650764941abfb24d1033
RudicoldbirdDE <96434134+RudicoldbirdDE@users.noreply.github.com>
2021-12-30 01:02:30 +0100

    Added Team Members Text v0.1.21
    Added Team Members Text v0.1.21

--- src/components/Team.vue
+++ src/components/Team.vue
@@ -3,7 +3,44 @@
    <div class="teamFilter">
      <div class="container title_top5BG_top5SM">
        <div class="offset-xl-2 col-xl-8 col-10 offset-1 text_center fonts_50" style="text-align: center; font-family: 'Dosis', sans-serif; color: #c7c7c7">
-         <span class="colorHighlight">Our Team</span>  integrated in your Projects
+         <span class="colorHighlight">Software Engineers</span>  integrated in your Projects
+       </div>
+       <div class="container-fluid engineers_avatar">
+         <div class="row justify-content-md-center">
+           <div class="offset-1 xxl-col-3 col-3">
+             <div class="aCircle" id="jAvatar">
+             </div>
+             <div class="teamTextrow">
+               <div class="teamText">
+                 Justin B. is responsible for technical marketing and search engine optimization (SEO).
+                 In addition, he honors JS, VueJS, CSS3 and HTML5 standards.
+                 He also has a knack for handling complex user environments (UI) and enhancing the user experience.
+               </div>
+             </div>
+           </div>
+           <div class="offset-1 xxl-col-3 col-3">
+             <div class="aCircle" id="rAvatar">
+             </div>
+             <div class="teamTextrow">
+               <div class="teamText">
+                 Rudolf A. is our Managing Director and Senior Software Engineer.
+                 He is familiar with search engine optimization (SEO), VueJS, Svelte, Electron and the HTML5 and CSS3 standards as well as the management and development of distributed backend solutions.
+                 He also has advanced GOlang, Tensorflow (C++) as well as C# (Core.NET) skills.
+               </div>
+             </div>
+           </div>
+           <div class="offset-1 xxl-col-3 col-3">
+             <div class="aCircle" id="dAvatar">
+             </div>
+             <div class="teamTextrow">
+               <div class="teamText">
+                 Dillon M. is our backend software engineer.
+                 He develops all server-side applications and tailors them to our projects.
+                 He has very good knowledge in GOlang, Tensorflow (C++) and also has the necessary skills to develop our Wordpress CMS systems as deocpled architecture.
+               </div>
+             </div>
+           </div>
+         </div>
        </div>
      </div>
    </div>
