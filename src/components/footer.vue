5fa730b7174ab608636211687d9ad424b4bf2582
RudicoldbirdDE <96434134+RudicoldbirdDE@users.noreply.github.com>
2022-01-04 02:36:05 +0100

    added Center Social Icons
    added Center Social Icons

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="footer_main" style="height: 20vh; background-color: #0e0e10">
-    <div class="container">
+  <div class="footer_main" style="min-height: 20vh; background-color: #0e0e10; position: relative">
+    <div class="container-fluid">
       <div class="row justify-content-md-center footer_item">
-        <div class="col-xxl-3">
+        <div class="col-xxl-3 offset-xxl-3 offset-sm-0 col-12">
           <ul id="footer_list">
             <li class="listItem">
               <div class="other_item"></div>Imprint
@@ -15,17 +15,29 @@
             </li>
           </ul>
         </div>
-        <div class="col-xxl-3">
+        <div class="col-xxl-3 offset-xxl-3 col-6 offset-3 col-md-4 social">
           <div id="socialsHeader">
             ... find us on ...
           </div>
-          <div id="socials">
-            <div id="twitter_icon"></div>
-            <div id="facebook_icon"></div>
-            <div id="instagram_icon"></div>
+          <div id="socials" >
+            <div id="twitter_icon" class="ficon">
+              <img alt="Twitter Icon" src="../assets/icons/twitter.svg" style="width: 45px; pointer-events: none;">
+            </div>
+            <div id="facebook_icon" class="ficon">
+              <img alt="Twitter Icon" src="../assets/icons/facebook.svg" style="width: 45px; pointer-events: none;">
+            </div>
+            <div id="instagram_icon" class="ficon">
+              <img alt="Twitter Icon" src="../assets/icons/instagram.svg" style="width: 45px; pointer-events: none;">
+            </div>
           </div>
         </div>
-        <div class="col-xxl-3"></div>
+      </div>
+    </div>
+    <div class="container-fluid brights">
+      <div class="row justify-content-md-center">
+        <div class="col-xxl-12">
+          &copy; Coldbird 2022 - All Rights reserved
+        </div>
       </div>
     </div>
   </div>
