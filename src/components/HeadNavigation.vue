3332a898bc6b28ebbec3b1fc3de7b9404fc2fe42
RudicoldbirdDE <96434134+RudicoldbirdDE@users.noreply.github.com>
2022-01-05 02:50:13 +0100

    v0.2.1 Added Imprint
    v0.2.1 Added Imprint

--- src/components/HeadNavigation.vue
+++ src/components/HeadNavigation.vue
@@ -2,7 +2,11 @@
   <div class="navigation_wrapper">
     <div class="container-fluid">
       <div class="row">
-        <div class="offset-xxl-1 col-xxl-1 col-1 navigation_title"><span style="color: #2a94f4">Cold</span>bird</div>
+        <div class="offset-xxl-1 col-xxl-1 col-1 navigation_title">
+          <router-link to="/" id="rDefault">
+            <span style="color: #2a94f4">Cold</span>bird
+          </router-link>
+        </div>
         <div class="offset-xxl-6 col-xxl-3 offset-8 col-1 navigation_item">
           <img alt="Mobile Navigation Bar" class="mobileBar" src="../assets/icons/bars.svg" style="border-radius: 3px; border: 1px solid green; padding-right: 5px; padding-left: 5px; height: 30px; filter: invert(47%) sepia(90%) saturate(2055%) hue-rotate(188deg) brightness(97%) contrast(96%);">
         </div>
@@ -13,7 +17,7 @@
 
 <script>
 export default {
-  name: "Header"
+  name: "HeadNavigation"
 }
 </script>
 
--- src/views/Imprint.vue
+++ src/views/Imprint.vue
@@ -1,5 +1,56 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="wrapper" style="overflow-x: hidden;">
+    <div class="background_wp">
+      <div class="background" style="position: relative">
+        <HeadNavigation style="position: absolute; bottom: 0; width: 100%"></HeadNavigation>
+      </div>
+    </div>
+    <div class="content">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="offset-xl-3 col-xl-3">
+            <div class="head_title">
+              Imprint
+            </div>
+            <div class="head_description">
+              Transparency is our top priority
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="container-fluid">
+        <div class="row">
+          <div class="offset-xl-3 col-xl-3 content_handle">
+            <div class="content_address">
+              <span class="address_title">Address | Responsible</span>
+              <div class="address_text">
+                <ul style="list-style: none" id="address_list">
+                  <li>Coldbird</li>
+                  <li>On request</li>
+                  <li>admin@coldbird.cc</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <Footer></Footer>
   </div>
 </template>
+<script>
+import HeadNavigation from "@/components/HeadNavigation";
+import Footer from "@/components/footer";
+
+export default {
+  name: "Imprint",
+  components: {
+    HeadNavigation,
+    Footer
+  }
+}
+</script>
+
+<style scoped>
+@import "../assets/css/policy.css";
+</style>
